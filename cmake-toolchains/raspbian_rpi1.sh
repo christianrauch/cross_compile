54185412bdd4495d5b45173b8b2721613fcfda7b
Christian Rauch <Rauch.Christian@gmx.de>
2019-08-03 01:22:37 +0100

    docker env and cmake toolchain

---
+++ cmake-toolchains/raspbian_rpi1.sh
@@ -0,0 +1,10 @@
+export TARGET_ARCH=arm
+
+export CROSS_COMPILER_C="arm-linux-gnueabihf-cc"
+export CROSS_COMPILER_CXX="arm-linux-gnueabihf-c++"
+
+export SYSROOT="/root/sysroot"
+export PYTHON_SOABI="cpython-36m-arm-linux-gnueabihf"
+
+export TARGET_C_FLAGS="-mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -w -O2 -Wl,-rpath-link=/root/ws/install/lib"
+export TARGET_CXX_FLAGS="-mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -w -O2 -Wl,-rpath-link=/root/ws/install/lib"
